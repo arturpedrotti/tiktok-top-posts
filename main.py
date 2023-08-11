de596d6857249262f8964c0d23d038e71864e856
arturpedrotti <arturgrochau@gmail.com>
2023-08-11 14:28:49 -0300

    O programa foi mudado para exibir os 5 toktoks mais vizualizados

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
-data_url = "https://raw.githubusercontent.com/mateuspestana/datasets_aulas/main/ukraine.csv"
-data = pd.read_csv(data_url)
+def fetch_tiktok_data():
+    data_url = "https://raw.githubusercontent.com/mateuspestana/datasets_aulas/main/ukraine.csv"
+    data = pd.read_csv(data_url)
+    return data
 
-print(data)
+def display_data(data):
+    # Modifc ade acordo com a ordem da coluna
+    print("Top 5 TokTok Posts:")
+    for index, row in data.head(5).iterrows():
+        views = f"{row['views']:,.0f}"  # Format views with commas
+        print(f"{index + 1}. Username: {row['user']} | URL: {row['url']} | Views: {views}")
+
+data = fetch_tiktok_data()
+display_data(data)
+
